f6784b6206305dff925c0cf4f3922cec6128e22e
Robertiu <RobertoVargasrdz46@gmail.com>
2026-03-04 21:32:57 -0600

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,11 @@
--- Create user with password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- Create database if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- Grant all privileges on the server
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- Create user if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege only on the database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 -- Apply changes
 FLUSH PRIVILEGES;
